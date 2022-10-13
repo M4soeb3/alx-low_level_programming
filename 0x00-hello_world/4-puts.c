ec1f4349da42b99705ce1903c4c02a1ce5071b90
Simon <litshitsomasoebe@gmail.com>
2022-10-13 11:23:46 -0700

    C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+ * "Programming is like building a multilingual puzzle" is to be printed by main
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
