f482f108c06be1c8ec064bd950b837e0aefd7cbd
Simon <litshitsomasoebe@gmail.com>
2022-10-14 09:03:06 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a serie of numbers with commas
+ * 
+ * Return: Always (Success);
+ */
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+
+		if (c != 
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+		putchar('\n');
+		return (0);
+}
+
