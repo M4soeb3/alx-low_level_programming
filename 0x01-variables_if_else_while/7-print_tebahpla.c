9aa348cadc24979ca6e5b0b0fdfc132bc4a699a1
Simon <litshitsomasoebe@gmail.com>
2022-10-14 08:35:01 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - alphabet in lowercase letters in reverse
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph;
+
+	for (alph = 'z'; alph >= 'a'; aplh--)
+	putchar(alph);
+
+	putchar('\n');
+
+	return (0);
+}
