199e7f33eb1876ef944282596d6b4d0f4e65e1f6
Simon <litshitsomasoebe@gmail.com>
2022-10-18 13:32:33 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - print aplhabet
+ * Return (0) Success
+ */
+
+void print_alphabet(void)
+{
+	char alph = 'a';
+
+	while (alph <= 'z')
+	{
+		_putchar (alph);
+		aplh++;
+	}
+	_putchar ('\n');
+}
