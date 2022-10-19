a6812a5bd89192b6a1f8ef044c341cb48cea201a
Simon <litshitsomasoebe@gmail.com>
2022-10-19 10:47:08 -0700

    Is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,9 @@
 
 /**
  * _islower - returns 1 if an alphabet is lower case
+ * @c: the character to check
  *
+ * Return: Always 0
  */
 
 int _islower(int c)
@@ -10,5 +12,5 @@ int _islower(int c)
 	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
-		return(0);
+		return (0);
 }
