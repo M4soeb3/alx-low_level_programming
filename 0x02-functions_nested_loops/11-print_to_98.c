de22b5bce5866043c0e640b9d6a4e33ab3353322
Simon <litshitsomasoebe@gmail.com>
2022-10-19 13:09:34 -0700

    Absolute

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints natural
+ *
+ * @n: print from this numbe
+ */
+
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+	while (n > 98)
+		printf("%d, ", n--);
+
+		printf("%d, ", n--);
+	}
+
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+
+		printf("%d\n", n);
+	}
+}
