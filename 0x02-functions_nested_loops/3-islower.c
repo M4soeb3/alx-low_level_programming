29af61884eb3fc491fec06c5bcf36ebd1fefe8d4
Simon <litshitsomasoebe@gmail.com>
2022-10-19 00:39:46 -0700

    Check lower cases

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - returns 1 if an alphabet is lower case
+ *
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		retrurn (1);
+	else
+		return(0);
+}
