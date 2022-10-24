bb955c9459db893930fb4c89a1daefadd8694ffa
Simon <litshitsomasoebe@gmail.com>
2022-10-24 14:29:01 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - print line of variable length
+ * @n: length of line
+ * Return: success
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+			_putchar('_');
+	}
+	_putchar('\n');
+
+}
